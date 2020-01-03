c16e963a0b3f89dcf911398d36da5e78e6836401
Estevao <estevaovasques@hotmail.com>
2020-01-02 22:14:12 -0300

    Ajustado retorno para react

--- app/Controllers/Http/InstaluraController.js
+++ app/Controllers/Http/InstaluraController.js
@@ -1,44 +1,68 @@
-'use strict'
-const posts = [{
-    urlPerfil: '',
-    loginUsuario: 'rafael',
-    horario: '05/12/2016 16:21',
-    urlFoto: '',
-    id: 3,
-    likeada: false,
-    likers: [],
-    comentarios: [
-        {
-           login: 'alots',
-           texto: 'novo comentario',
-           id: 1
-        },
-        {
-            login: 'alots',
-            texto: 'novo comentario',
-            id: 1
-        }
-    ],
-    comentario: 'comentario da foto'
-}]
+"use strict";
+const posts = [
+    {
+        id: 3,
+        urlPerfil: "",
+        loginUsuario: "Mantega",
+        horario: "05/12/2016 16:21",
+        urlFoto: "",
+        likeada: false,
+        likers: [],
+        comentarios: [
+            {
+                login: "alots",
+                texto: "novo comentario",
+                id: 1
+            },
+            {
+                login: "alots",
+                texto: "outro comentario",
+                id: 2
+            }
+        ],
+        comentario: "comentario da foto"
+    },
+    {
+        id: 4,
+        urlPerfil: "",
+        loginUsuario: "Mantega",
+        horario: "05/12/2016 16:21",
+        urlFoto: "",
+        likeada: false,
+        likers: [],
+        comentarios: [
+            {
+                login: "alots",
+                texto: "novo comentario",
+                id: 4
+            },
+            {
+                login: "alots",
+                texto: "outro comentario",
+                id: 5
+            }
+        ],
+        comentario: "comentario da foto"
+    }
+];
 
 class InstaluraController {
     async index() {
         try {
-            return posts
+            return posts;
         } catch (err) {
-            console.error(err)
-            return response.status(500).send({ error: err.message })
+            console.error(err);
+            return response.status(500).send({ error: err.message });
         }
     }
 
-    async store ({ request, response }) {}
+    async store({ request, response }) {}
 
-    async show ({ params, request, response, view }) {}
+    async show({ params, request, response, view }) {}
 
-    async update ({ params, request, response }) {}
+    async update({ params, request, response }) {}
 
-    async destroy ({ params, request, response }) {}
+    async destroy({ params, request, response }) {}
 }
 
-module.exports = InstaluraController
+module.exports = InstaluraController;
